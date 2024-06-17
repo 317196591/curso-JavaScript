cda1d874b1644ff2e6591a343c9e8af5d696c369
alexromerormz <alexromerormz@gmail.com>
2024-06-17 11:09:49 -0600

    Ejercicio 1

---
+++ EJERCICIOS JS ALUMNOS/01-Introduccion/js/index.js
@@ -0,0 +1,9 @@
+console.log("Curso Javascript desde archivo externo")
+const hola = "Hola desde la Consola"
+console.log(hola)
+console.log([1,2,3,4])
+const numero = 12
+console.log(numero)
+let obj = {nombre : 'alex',profesion:'ingeniero',edad:20}
+console.log(obj)
+console.table([1,2,3])
