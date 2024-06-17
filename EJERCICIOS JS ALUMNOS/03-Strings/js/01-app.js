ab59d9b5bb8150ba1b9fcb8de90e3a0725fc73f2
alexromerormz <alexromerormz@gmail.com>
2024-06-17 11:50:56 -0600

    Ejercicio 3 2

--- EJERCICIOS JS ALUMNOS/03-Strings/js/01-app.js
+++ EJERCICIOS JS ALUMNOS/03-Strings/js/01-app.js
@@ -1,6 +1,6 @@
 const producto = 'Monitor'
 console.log(producto)
-const producto2 = String('Monitor de 24 pulgadas')
+const producto2 = String(24)
 console.log(producto2)
 const numero = 12
 console.log(numero)
